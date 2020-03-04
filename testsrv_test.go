7112441a7652cd78af3b01a5525a69c633891154
Jack Hayhurst <jhayhurst@liquidweb.com>
2020-03-04 16:23:53 -0500

    wrote base test functions
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

--- testsrv_test.go
+++ testsrv_test.go
@@ -1,24 +1,64 @@
 package cpanel
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"sort"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 var cpanelTestSrv httptest.Server
 var mux http.ServeMux
 
-func FormatQueryParams(values url.Values) string {
-	var mapArgs []string
+var testAuthToken = "/cpsess8675309"
 
-	for key, array := range values {
-		sort.StringSlice(array)
-		mapArgs = append(mapArgs, key+"_"+strings.Join(array, "-"))
-	}
+// This handler checks for the API version and returns a 404 if not found.
+func StripApiVersion(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if len(r.URL.Query.Get("api.version")) != 1 ||
+			r.URL.Query.Get("api.version")[0] != "1" {
+			http.NotFound(w, r)
+			return
+		}
+		r.URL.Query.Delete("api.version")
+		h.ServeHTTP(w, r)
+	})
+}
 
-	sort.StringSlice(mapArgs)
-	return strings.Join(mapArgs, ".")
+func StripToken(authHandler http.Handler, unauthHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if p := strings.TrimPrefix(r.URL.Path, testAuthToken); len(p) < len(r.URL.Path) {
+			r2 := new(Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r.URL
+			r2.URL.Path = p
+			authHandler.ServeHTTP(w, r2)
+		} else {
+			unauthHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func RespondWithFile(pathPrefix string, notFoundCode int) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open(filepath.Join(
+			strings.TrimPrefix(pathPrefix, "/"),
+			r.URL.Path,
+			r.URL.Query().Encode,
+		))
+		if err != nil && os.IsNotExist(err) {
+			http.Error(w, notFoundCode, "")
+		} else if err != nil {
+			http.Error(w, http.StatusInternalServerError, "internal problem")
+		} else {
+			_, err = io.Copy(w, f)
+			if err != nil {
+				http.Error(w, notFoundCode, "")
+			}
+		}
+	})
 }
